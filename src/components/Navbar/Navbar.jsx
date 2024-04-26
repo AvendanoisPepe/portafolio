4d0b53144cfd8eaa423370878f0aa55a95cb27ac
AvendanoisPepe <jsavendano62@misena.edu.co>
2024-04-26 00:09:24 -0500

    estructura menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,71 +1,92 @@
 import './styles.scss'
-import projectNavbar from './dataNavbar'
-import { useEffect, useState } from 'react'
-import DropDown from './DropDown'
-import LinkRoute from './LinkRoute'
-import imgLogo from '../../assets/images/index/logoMain.png'
-import IconSearch from '../../icons/IconSearch'
+// import projectNavbar from './dataNavbar'
+// import { useEffect, useState } from 'react'
+// import DropDown from './DropDown'
+// import LinkRoute from './LinkRoute'
+// import imgLogo from '../../assets/images/index/logoMain.png'
+// import IconSearch from '../../icons/IconSearch'
 
 export default function Navbar() {
-	const [openNav, setOpenNav] = useState(false)
-	const [dataSearch, setDataSearch] = useState('')
+	// const [openNav, setOpenNav] = useState(false)
+	// const [dataSearch, setDataSearch] = useState('')
 
-	const openCloseNavBar = () => {
-		if (openNav) return setOpenNav(!openNav)
-		return setOpenNav(!openNav)
-	}
+	// const openCloseNavBar = () => {
+	// 	if (openNav) return setOpenNav(!openNav)
+	// 	return setOpenNav(!openNav)
+	// }
 
-	const search = valueSearch => {
-		setDataSearch(valueSearch)
-		const allCards = document.querySelectorAll('.dato-buscado')
-		const mobileCards = Array.from(allCards).filter(card => card.textContent.includes(valueSearch))
-		Array.from(allCards).forEach(card => {
-			card.classList.add('hide')
-		})
-		mobileCards.forEach(card => {
-			card.classList.remove('hide')
-		})
-	}
+	// const search = valueSearch => {
+	// 	setDataSearch(valueSearch)
+	// 	const allCards = document.querySelectorAll('.dato-buscado')
+	// 	const mobileCards = Array.from(allCards).filter(card => card.textContent.includes(valueSearch))
+	// 	Array.from(allCards).forEach(card => {
+	// 		card.classList.add('hide')
+	// 	})
+	// 	mobileCards.forEach(card => {
+	// 		card.classList.remove('hide')
+	// 	})
+	// }
 
 	return (
-		<header className={'sidebar ' + openNav}>
-			{/* {cabcera buscador logo} */}
-			<div className="sidebar__logobx">
-				<figure className="sidebar__logobx--imgbx" id="navBar_btn" onClick={openCloseNavBar}>
-					<img src={imgLogo} alt="logo" />
-				</figure>
-				<span className="sidebar__logobx--name">Automatizaciones Formación</span>
-				<div className="input-container">
-					<input
-						type="text"
-						name="text"
-						value={dataSearch}
-						className="input"
-						placeholder="Search something..."
-						id="searcher"
-						autoComplete="off"
-						onChange={e => {
-							search(e.target.value)
-						}}
-					/>
-					<IconSearch className="icon" />
-				</div>
-			</div>
-
-			{/* {contenido} */}
-			<ul className="sidebar__links">
-				{projectNavbar &&
-					projectNavbar.map((link, i) => {
-						if (link.dropDown) {
-							return <DropDown key={i} link={link} />
-						} else {
-							return <LinkRoute key={i} link={link} />
-						}
-					})}
-			</ul>
-
-			{/* {version} */}
-			<div className="sidebar__versionbx" id="sideVersion"></div>
+		<header>
+			<section>
+				<h1>Sebastian Avendaño</h1> <h2>Gonzalez</h2>
+			</section>
+			<section>
+				<ul>
+					<li>Sobre mi</li>
+					<li>Experiencia</li>
+					<li>Proyectos</li>
+					<li>Educacion</li>
+					<li>Habilidades</li>
+				</ul>
+			</section>
+			<section>
+				<ul>git</ul>
+				<ul>linkedin</ul>
+				<ul>gmail</ul>
+				<ul>git</ul>
+				<ul>contacto</ul>
+			</section>
 		</header>
+		// <header className={'sidebar ' + openNav}>
+		// 	{/* {cabcera buscador logo} */}
+		// 	<div className="sidebar__logobx">
+		// 		<figure className="sidebar__logobx--imgbx" id="navBar_btn" onClick={openCloseNavBar}>
+		// 			<img src={imgLogo} alt="logo" />
+		// 		</figure>
+		// 		<span className="sidebar__logobx--name">Automatizaciones Formación</span>
+		// 		<div className="input-container">
+		// 			<input
+		// 				type="text"
+		// 				name="text"
+		// 				value={dataSearch}
+		// 				className="input"
+		// 				placeholder="Search something..."
+		// 				id="searcher"
+		// 				autoComplete="off"
+		// 				onChange={e => {
+		// 					search(e.target.value)
+		// 				}}
+		// 			/>
+		// 			<IconSearch className="icon" />
+		// 		</div>
+		// 	</div>
+
+		// 	{/* {contenido} */}
+		// 	<ul className="sidebar__links">
+		// 		{projectNavbar &&
+		// 			projectNavbar.map((link, i) => {
+		// 				if (link.dropDown) {
+		// 					return <DropDown key={i} link={link} />
+		// 				} else {
+		// 					return <LinkRoute key={i} link={link} />
+		// 				}
+		// 			})}
+		// 	</ul>
+
+		// 	{/* {version} */}
+		// 	<div className="sidebar__versionbx" id="sideVersion"></div>
+		// </header>
 	)
 }
