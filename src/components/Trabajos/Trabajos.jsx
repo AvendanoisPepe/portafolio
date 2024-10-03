bc22a32ff9f12c077c7419f459b0ede283f557a6
AvendanoisPepe <jsavendano62@misena.edu.co>
2024-10-02 19:59:31 -0500

    aca se empiza los proyectos indi

--- src/components/Trabajos/Trabajos.jsx
+++ src/components/Trabajos/Trabajos.jsx
@@ -24,37 +24,37 @@ export default function Trabajos() {
 					<div className="contexto">
 						<h2>Web Training Televentas Colombia</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nesciunt at,
-							fugiat veniam aliquam labore possimus. Officiis laudantium numquam nobis aut quia
-							aliquam vitae, suscipit quisquam, accusamus maiores sequi quidem.
+							Este sistema lo cree con mucho cariño ya que fue la primera vez que realice un proyecto con graficas comparativas, en este caso de dos
+							difentes planes de datos, como era la primera vez se generon varias actualizaciones al mismo desarrollo para generar la mejor version, el proyecto tambien
+							incluye Scripts y argumentarios de ventas.
 						</p>
 					</div>
 					<div className="linea"></div>
 					<div className="tabs">
-						<h2>2022 - Presente</h2>
+						<h2>2024 - Presente</h2>
 						<ul>
-							<li>HTML - SASS</li>
+							<li>CHART.JS</li>
 							<li>JAVASCRIPT</li>
-							<li>SWEETALERT2</li>
+							<li>REACT</li>
+							<li>WEBPACK</li>
 						</ul>
 					</div>
 				</div>
 				<div className="trabajo">
 					<div className="tabs">
-						<h2>2022 - Presente</h2>
+						<h2>2024 - Presente</h2>
 						<ul>
-							<li>HTML - SASS</li>
+							<li>JSX - SASS</li>
 							<li>JAVASCRIPT</li>
-							<li>SWEETALERT2</li>
+							<li>REACT</li>
+							<li>WEBPACK</li>
 						</ul>
 					</div>
 					<div className="linea"></div>
 					<div className="contexto">
 						<h2>Web Training Claro TMK</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nesciunt at,
-							fugiat veniam aliquam labore possimus. Officiis laudantium numquam nobis aut quia
-							aliquam vitae, suscipit quisquam, accusamus maiores sequi quidem.
+							En este proyecto se perfeccionaron las graficas ya que se genero un nuevo sistema donde se comparan multiples caractericas de dos celulares distintos y se especifica cual de los dos casos es mejor, de igual forma se contempla diferentes desarrollos para el beneficio del usuario.
 						</p>
 					</div>
 				</div>
@@ -62,18 +62,17 @@ export default function Trabajos() {
 					<div className="contexto">
 						<h2>Web Training JetSmart</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nesciunt at,
-							fugiat veniam aliquam labore possimus. Officiis laudantium numquam nobis aut quia
-							aliquam vitae, suscipit quisquam, accusamus maiores sequi quidem.
+							Sistema con el objetivo de visualizar la mayor cantidad de casos posibles a una situacion respectiva (Scripts variados), con el mismo objetivo se creo el ayudante de comandos, los casos DINSIDE, Glosarios, tipificadores, donde cada uno cumple el objetivo que una persona nueva en el area tenga una ruta exacta y detallada para solucionar la necesidad del momento.
 						</p>
 					</div>
 					<div className="linea"></div>
 					<div className="tabs">
-						<h2>2022 - Presente</h2>
+						<h2>2024 - Presente</h2>
 						<ul>
-							<li>HTML - SASS</li>
+							<li>JSX - SASS</li>
 							<li>JAVASCRIPT</li>
-							<li>SWEETALERT2</li>
+							<li>REACT</li>
+							<li>WEBPACK</li>
 						</ul>
 					</div>
 				</div>
@@ -84,15 +83,15 @@ export default function Trabajos() {
 							<li>HTML - SASS</li>
 							<li>JAVASCRIPT</li>
 							<li>SWEETALERT2</li>
+							<li>JQUERY</li>
 						</ul>
 					</div>
 					<div className="linea"></div>
 					<div className="contexto">
 						<h2>Web Training Colpatria</h2>
 						<p>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nesciunt at,
-							fugiat veniam aliquam labore possimus. Officiis laudantium numquam nobis aut quia
-							aliquam vitae, suscipit quisquam, accusamus maiores sequi quidem.
+							{/* El primer proyecto grande que trabaje en la empresa actual en la que trabajo, como se estaba iniciando se genero a base de tecnologias basicas pero se busco un diseño agradable con el usuario basado en la b */}
+							Sistema generado para ser una gran biblioteca de informacion donde su principal funcion es la consulta de documentos y procesos (CheckList, Generadores de notas), con un buscador general y un menu diseñado por secciones principales y submenus para que estuviera bien distribuida la informacion.
 						</p>
 					</div>
 				</div>
